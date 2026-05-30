e2f646c518c8eacf75940ca8c75000edafa258bb
Kate Saunders <kate.saunders@monash.edu>
2026-05-30 12:01:57 +1000

    Some website updates and tweaks

--- course_info.R
+++ course_info.R
@@ -20,7 +20,7 @@ schedule <- tribble(
     8, "Case Study: Data ethics", "", "",
     9, "Case Study: Data ethics and privacy", "", "",
     10, "Case Study: Introduction to web scraping", "", "", #Introduction to webscraping
-    11, "Case Study: TBC", "", "", #Large Language Models for preparing data in R
+    11, "Case Study: LLMs for data analysis", "", "", #Large Language Models for preparing data in R
     12, "Revision: Proper care and feeding of wild caught data", "", "", 
 )
 
